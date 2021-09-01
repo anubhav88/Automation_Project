38186709a042ae5a22c9c87c765467f786b5c754
anubhav88 <anubhavkapoor88@hotmail.com>
2021-09-01 15:11:13 +0530

    Add files via upload

--- automation.sh
+++ automation.sh
@@ -27,7 +27,46 @@ echo "Creating Tar file "
 
 tar -cf ${filename} $( find /var/log/apache2/ -name "*.log")
 
+
+filesize=$(wc -c $filename | awk '{print $1}')
+
 echo "uploading to S3"
 aws s3 cp ${filename} s3://${s3_bucket}/${filename}
 
 echo "uploading to S3 done"
+
+inventory_file="/var/www/html/inventory.html"
+
+echo "checking inventory file"
+
+if [[ ! -f $inventory_file ]]; then
+    echo "Invetory file not found creating one" 
+    sudo touch $inventory_file
+    sudo chmod 777 $inventory_file
+    sudo echo "Log Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Time Created&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Type&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Size<br>" >> $inventory_file
+fi
+
+sudo echo "httpd-logs&nbsp;&nbsp;&nbsp;&nbsp;$timestamp&nbsp;&nbsp;&nbsp;&nbsp;tar&nbsp;&nbsp;&nbsp;&nbsp;$filesize Bytes<br>" >> $inventory_file
+
+echo "inventory file updated"
+
+cron_file="/etc/cron.d/automation"
+automation_file="/root/Automation_Project/automation.sh"
+
+echo "checking cron job"
+
+cron_job_exists=$(sudo crontab -l | grep 'automation')
+
+echo "cron job found : $cron_job_exists"
+
+if [[ ! $cron_job_exists ]]; then
+	if [[ ! -f  $cron_file ]]; then
+		echo "cron_file file not found creating one"
+		sudo touch $cron_file
+		sudo chmod 777 $cron_file
+		sudo echo "00 11 * * * $automation_file" >> $cron_file
+	fi
+
+	echo "registering cronjob"
+	sudo crontab $cron_file
+fi
